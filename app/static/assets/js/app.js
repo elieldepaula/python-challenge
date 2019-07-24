6db44467f08d0bb0c8833146c1b71ba92e8d184f
Eliel de Paula <elieldepaula@gmail.com>
2019-07-24 14:37:51 -0300

    List containers in front-end.

--- app/static/assets/js/app.js
+++ app/static/assets/js/app.js
@@ -1,12 +1,30 @@
 $(document).ready(function () {
 
-    $.get("/api/list").done(function (data) {
+    /* $.get("/api/list").done(function (data) {
         for(row in data){
             console.log(row.containers.id);
         }
         // console.log(data);
     }).fail(function(){
         alert("Errouu!")
-    });
+    });*/
+
+    listContainers();
+
 
 });
+
+/**
+ * List all containers into front-end.
+ */
+function listContainers()
+{
+    $.getJSON("/api/list").done(function(data){
+        $.each(data function(i, item){
+            $('td') .html(
+                $('td').text(item.id),
+                $('td').text(item.name)
+            ).appendTo("#tableRows");
+        })
+    });
+}
